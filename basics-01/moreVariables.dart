c4e06652267e5569c2479cb95992b71c8097a1b6
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-10-01 19:16:08 +0530

    Null, dynamic, final and const varibale practice

--- basics-01/moreVariables.dart
+++ basics-01/moreVariables.dart
@@ -1,13 +1,34 @@
 // final, const and var
 
 void main() {
-  var a = 10;
-  final b = 20;
-  const c = 30;
+  var a = 10; //-> mutable but type inferred // Can change type
+  final b = 20; // value set once, but decided in run time
+  const c = 30; // value set once but decided in compile time
 
   a = 15;
   // b = 25; error
   // c = 45; error
 
   print("$a $b $c");
+  nullFunctions();
+}
+
+// Nullable and non-nullable variable
+
+void nullFunctions() {
+  String name = "John";
+  // name = null; error -> Can't assign null to a without check null, assigned variable
+
+  String? title = "Shaw";
+  title = null;
+  print("$name $title");
+  dynamicDataType();
+}
+
+//dynamic
+
+void dynamicDataType() {
+  dynamic x = "Hello";
+  x = 1234; // Can change type
+  print("$x");
 }
