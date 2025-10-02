ca870088781836eac6a5262495261f2bee8fe404
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-10-02 19:34:28 +0530

    Learned about operators

--- basics-01/operators.dart
+++ basics-01/operators.dart
@@ -1,5 +1,10 @@
+//Arithmetic Operator
+
 void main() {
   arithmeticOperator();
+  comparision();
+  logical();
+  assignment();
 }
 
 void arithmeticOperator() {
@@ -12,4 +17,42 @@ void arithmeticOperator() {
   print(a % b);
   print(a ~/ b);
   print(a / b);
+}
+
+// Comparision Operator
+
+void comparision() {
+  int a = 20;
+  int b = 30;
+
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+  print(a != b);
+  print(a == b);
+}
+
+// Logical Operator
+
+void logical() {
+  bool x = true;
+  bool y = false;
+
+  print(x && y);
+  // print(x || y);
+  print(!x);
+}
+
+// Assignment Operator
+
+void assignment() {
+  int a = 10;
+
+  print(a += 5);
+  print(a -= 5);
+  print(a *= 5);
+  print(a = 10);
+  print(a %= 4);
+  print(a ~/= 4);
 }
